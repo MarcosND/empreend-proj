2ca5e4f38b28112ff7b3f8e4e9debac264865211
Caio Bezerra Londres Barreto <caiobarreto_28@hotmail.com>
2025-03-12 22:45:46 -0300

    chore: fix dependencies and uncomment code

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider as PaperProvider } from 'react-native-paper';
-import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
-import HomeScreen from './screens/HomeScreen';
-import SplashScreen from './screens/SplashScreen';
-import ConsultaScreen from './screens/ConsultaScreen';
-import AtividadeScreen from './screens/AtividadeScreen';
-import RegulacaoEmocionalScreen from './screens/RegulacaoEmocionalScreen'
-import IdentificacaoEmocoesScreen from './screens/IdentificacaoEmocoesScreen'
-// import AtividadeDetalhesScreen from './screens/AtividadeDetalhesScreen';
-// import PacienteDetalhesScreen from './screens/PacienteDetalhesScreen';
+import "react-native-gesture-handler";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Provider as PaperProvider } from "react-native-paper";
+import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
+import HomeScreen from "./screens/HomeScreen";
+import SplashScreen from "./screens/SplashScreen";
+import ConsultaScreen from "./screens/ConsultaScreen";
+import AtividadeScreen from "./screens/AtividadeScreen";
+import RegulacaoEmocionalScreen from "./screens/RegulacaoEmocionalScreen";
+import IdentificacaoEmocoesScreen from "./screens/IdentificacaoEmocoesScreen";
+import AtividadeDetalhesScreen from "./screens/AtividadeDetalhesScreen";
+import PacienteDetalhesScreen from "./screens/PacienteDetalhesScreen";
 
-import { theme } from './styles/theme';
+import { theme } from "./styles/theme";
 
 const Stack = createStackNavigator();
 
@@ -22,15 +22,30 @@ const App = () => {
     <NavigationContainer>
       <EmotionThemeProvider theme={theme}>
         <PaperProvider theme={theme}>
-          <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+          <Stack.Navigator
+            initialRouteName="Splash"
+            screenOptions={{ headerShown: false }}
+          >
             <Stack.Screen name="Splash" component={SplashScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Consulta" component={ConsultaScreen} />
             <Stack.Screen name="AtividadeScreen" component={AtividadeScreen} />
-            <Stack.Screen name="RegulacaoEmocionalScreen" component={RegulacaoEmocionalScreen} />
-            <Stack.Screen name="IdentificacaoEmocoesScreen" component={IdentificacaoEmocoesScreen} />
-            {/* <Stack.Screen name="AtividadeDetalhes" component={AtividadeDetalhesScreen} /> */}
-            {/* <Stack.Screen name="PacienteDetalhesScreen" component={PacienteDetalhesScreen} /> */}
+            <Stack.Screen
+              name="RegulacaoEmocionalScreen"
+              component={RegulacaoEmocionalScreen}
+            />
+            <Stack.Screen
+              name="IdentificacaoEmocoesScreen"
+              component={IdentificacaoEmocoesScreen}
+            />
+            <Stack.Screen
+              name="AtividadeDetalhes"
+              component={AtividadeDetalhesScreen}
+            />
+            <Stack.Screen
+              name="PacienteDetalhesScreen"
+              component={PacienteDetalhesScreen}
+            />
           </Stack.Navigator>
         </PaperProvider>
       </EmotionThemeProvider>
@@ -39,7 +54,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
